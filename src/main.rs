9d0ac450d6f90d72fb53bfbb223503d1ad18794c
Drew Vogel <dvogel@github>
2023-07-25 23:34:32 -0500

    Use libc sockaddr structs.

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ use libbpf_rs::skel::OpenSkel;
 use libbpf_rs::skel::Skel;
 use libbpf_rs::skel::SkelBuilder;
 use libbpf_rs::RingBufferBuilder;
-use libc; // TODO: replace custom sockaddr implementations with libc
 use plain::Plain;
 use std::mem;
 
@@ -19,81 +18,65 @@ mod certspook {
 
 use certspook::*;
 
-#[repr(C)]
-#[derive(Default, Copy, Clone)]
-struct in_addr {
-    pub s_addr: u32,
-}
+#[derive(Copy, Clone)]
+struct WrappedSockaddrIn(libc::sockaddr_in);
 
-#[repr(C)]
-#[derive(Default, Copy, Clone)]
-struct sockaddr_in {
-    pub sin_family: u16,
-    pub sin_port: u16,
-    pub sin_addr: in_addr,
-    pub sin_zero: [u8; 8],
-}
+unsafe impl Plain for WrappedSockaddrIn {}
 
-unsafe impl Plain for sockaddr_in {}
-
-impl sockaddr_in {
-    fn from_bytes(buf: &[u8]) -> &sockaddr_in {
-        plain::from_bytes(buf).expect("Buffer cannot be decoded into sockaddr_in.")
+impl WrappedSockaddrIn {
+    fn from_bytes(buf: &[u8]) -> WrappedSockaddrIn {
+        let mut addr: WrappedSockaddrIn =
+            *plain::from_bytes(buf).expect("Buffer cannot be decoded into sockaddr_in.");
+        // libc normally converts these from network byte order to host byte order. Since we copied
+        // them directly from the kernel we need to do so.
+        addr.0.sin_addr.s_addr = u32::from_be(addr.0.sin_addr.s_addr);
+        addr.0.sin_port = u16::from_be(addr.0.sin_port);
+        addr
     }
 
     fn bytelen() -> usize {
-        // return 16;
-        // TODO: Why does mem::size_of return 8?
-        return mem::size_of::<sockaddr_in>();
+        mem::size_of::<libc::sockaddr_in>()
     }
 }
 
-impl fmt::Display for sockaddr_in {
+impl fmt::Display for WrappedSockaddrIn {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // TODO: The bytes will be in host order in sockaddr_in so this is not portable.
         write!(
             f,
-            "{}.{}.{}.{}:{}",
-            (self.sin_addr.s_addr & 0xFF),
-            (self.sin_addr.s_addr >> 8) & 0xFF,
-            (self.sin_addr.s_addr >> 16) & 0xFF,
-            (self.sin_addr.s_addr >> 24) & 0xFF,
-            ((self.sin_port >> 8) & 0xFF) | ((self.sin_port << 8) & 0xFF00)
+            "{}:{}",
+            std::net::Ipv4Addr::from(self.0.sin_addr.s_addr),
+            self.0.sin_port
         )
     }
 }
 
-#[repr(C)]
-#[derive(Default, Copy, Clone)]
-struct sockaddr_in6 {
-    pub sin6_family: u16,
-    pub sin6_port: u16,
-    pub sin6_flowinfo: u32,
-    pub sin6_addr: [u8; 16],
-    pub sin6_scope_id: u32,
-}
+#[derive(Copy, Clone)]
+struct WrappedSockaddrIn6(libc::sockaddr_in6);
 
-unsafe impl Plain for sockaddr_in6 {}
+unsafe impl Plain for WrappedSockaddrIn6 {}
 
-impl sockaddr_in6 {
-    fn from_bytes(buf: &[u8]) -> &sockaddr_in6 {
-        plain::from_bytes(buf).expect("Buffer cannot be decoded into sockaddr_in.")
+impl WrappedSockaddrIn6 {
+    fn from_bytes(buf: &[u8]) -> WrappedSockaddrIn6 {
+        let mut addr6: WrappedSockaddrIn6 =
+            *plain::from_bytes(buf).expect("Buffer cannot be decoded into sockaddr_in.");
+        // libc normally converts the port from network byte order to host byte order. Since we
+        // copied them directly from the kernel we need to do so.
+        addr6.0.sin6_port = u16::from_be(addr6.0.sin6_port);
+        addr6
     }
 
     fn bytelen() -> usize {
-        // return 28;
-        // TODO: Can this be made to work?
-        return mem::size_of::<sockaddr_in6>();
+        mem::size_of::<libc::sockaddr_in6>()
     }
 }
 
-impl fmt::Display for sockaddr_in6 {
+impl fmt::Display for WrappedSockaddrIn6 {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
             "[{}]:{}",
-            std::net::Ipv6Addr::from(self.sin6_addr),
-            self.sin6_port,
+            std::net::Ipv6Addr::from(self.0.sin6_addr.s6_addr),
+            self.0.sin6_port,
         )
     }
 }
@@ -111,19 +94,18 @@ fn bump_memlock_rlimit() -> Result<()> {
     Ok(())
 }
 
-// These bytes should already be in big endian format.
 fn handle_record(bytes: &[u8]) -> i32 {
-    if bytes.len() == sockaddr_in::bytelen() {
-        let addr_in = sockaddr_in::from_bytes(bytes);
+    if bytes.len() == WrappedSockaddrIn::bytelen() {
+        let addr_in = WrappedSockaddrIn::from_bytes(bytes);
         println!("ipv4:{}", &addr_in);
-    } else if bytes.len() == sockaddr_in6::bytelen() {
-        let addr_in6 = sockaddr_in6::from_bytes(bytes);
+    } else if bytes.len() == WrappedSockaddrIn6::bytelen() {
+        let addr_in6 = WrappedSockaddrIn6::from_bytes(bytes);
         println!("ipv6:{}", &addr_in6);
     } else {
         eprintln!(
             "Unrecognized record with byte length of {}, sockaddr_in should have {}",
             bytes.len(),
-            sockaddr_in::bytelen()
+            WrappedSockaddrIn::bytelen()
         );
     }
     0
@@ -134,8 +116,8 @@ fn handle_lost_events(cpu: i32, count: u64) {
 }
 
 fn main() -> Result<()> {
-    println!("sockaddr_in: {} bytes", sockaddr_in::bytelen());
-    println!("sockaddr_in6: {} bytes", sockaddr_in6::bytelen());
+    println!("sockaddr_in: {} bytes", WrappedSockaddrIn::bytelen());
+    println!("sockaddr_in6: {} bytes", WrappedSockaddrIn6::bytelen());
 
     bump_memlock_rlimit()?;
 
